bc4faacce39a28e2972cc632354e6967d3d630ed
Gabriel Strain <gabriel.strain@manchester.ac.uk>
2024-03-04 15:08:56 +0000

    item gen

--- item_preparation/stimuli_generation.R
+++ item_preparation/stimuli_generation.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(MASS)
 library(Hmisc)
 library(bbplot)
-
+library(png)
 
 # for now, let's just use the same seed as previous work
 
@@ -25,11 +25,11 @@ slope_function <- function(my_desired_r) {
   
   my_sample_size = 128
   
-  mean_variable_1 = 0
+  mean_variable_1 = 5
   sd_variable_1 = 1
   
-  mean_variable_2 = 0
-  sd_variable_2 = 1
+  mean_variable_2 = 76
+  sd_variable_2 = 5
   
   mu <- c(mean_variable_1, mean_variable_2) 
   
@@ -48,7 +48,8 @@ slope_function <- function(my_desired_r) {
   slopes <- data_with_resid %>%
     mutate(slope_0.25 = 1-(0.25)^my_residuals) %>%
     mutate(slope_inverted = (1 + (0.25)^ my_residuals)-1) %>%
-    mutate(slope_inverted_floored = pmax(0.1,(1+(0.25)^my_residuals)-1))  
+    mutate(slope_inverted_floored = pmax(0.1,(1+(0.25)^my_residuals)-1)) %>%
+    mutate(typical = 0.033)
   
   return(slopes)
 }
@@ -57,31 +58,38 @@ slope_function <- function(my_desired_r) {
 
 plot_function <- function(slopes, my_desired_r, letter, size_value, theme) {
   
+  logo <- readPNG("item_preparation/BBC-LOGO-SMALL.png")
+  
   p <- ggplot(slopes, aes(x = V1, y = V2)) +
     scale_size_identity() +
     scale_alpha_identity() +
-    geom_point(aes(size =  4*(size_value+0.2), alpha = 1), shape = 16) +  
-    labs(x = "", y = "") +
-    theme +
-    theme(axis.text = element_blank()) +
-    theme(plot.margin = unit(c(0,0,0,0), "cm")) +
-    theme(legend.position = "None")
+    geom_point(aes(size =  6*(size_value + 0.3), alpha = 1), shape = 16) +  
+    geom_hline(yintercept = 68, size = 1, colour="#333333") +
+    geom_segment(x = 0, xend = 10, y = 66.2, yend = 66.2, size = 0.3, colour="#585858") +
+    bbc_style() +
+    theme(axis.text.x = element_blank(),
+          title = element_text(size = 14),
+          plot.subtitle = element_text(size = 11),
+          plot.caption = element_text(size = 9, hjust = -.1)) +
+    labs(title = "Lockdown Gains",
+         subtitle = "More Covid-19 lockdowns were associated\nwith a higher life expectancy among the elderly",
+         caption = "Source: NHS England") +
+    annotate("text", x = 3, y = 67, label = "Fewer lockdowns") +
+    annotate("text", x = 7, y = 67, label = "More lockdowns") +
+    coord_cartesian(clip = "off") +
+    annotation_raster(logo, xmin =  7.2, xmax = 8.7, ymin = 85, ymax = 87)
   
   ggsave(p,filename=paste0(counter, letter, ".png"),
          device = "png",
          bg = "white",
          path = "item_preparation/all_plots",
          units = "px",
-         width = 1000,
-         height = 1000,
+         width = 1500,
+         height = 1500,
   )
   
   return(p)
 }
-
-# create BBC and FT themes
-
-
 
 # create plots
 
@@ -89,15 +97,13 @@ counter = 1
 for(value in high_r) {
   
   slopes <- slope_function(value)
-  slopeN <- (slopes$slope_0.25)
+  slopeI <- (slopes$slope_inverted)
+  typical <- (slopes$typical)
   
-  plot_function(slopes, value, "heythere", slopeN, bbc_style())
+  plot_function(slopes, value, "atyp", slopeI, bbc_style())
+  plot_function(slopes, value, "typ", typical, bbc_style())
   
   if (counter > 0) {
     counter = counter + 1
   }
 }
-
-
-
-
